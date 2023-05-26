7a67ce6c3cbaa692a1df1cf570ec210c2f31f136
ehgur062300 <79880998+ehgur062300@users.noreply.github.com>
2023-05-26 17:50:43 +0900

    nginx configuration update

--- my_project/settings.py
+++ my_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '캡스톤 웹 서버',
+        'USER': 'ehgur062300',
+        'PASSWORD' : 'St4vStA2fvjDgSNAcctp',
+        'HOST' : 'database-1.cvwutdxwqb99.ap-northeast-2.rds.amazonaws.com',
+        'PORT' : '3306',
     }
 }
 
